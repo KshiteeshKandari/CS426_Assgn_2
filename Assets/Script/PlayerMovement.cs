9343accc9702a8189ce381fad5e4edd4c4ce9b7a
Rohitt6363 <rohitrocks6363@gmail.com>
2024-02-06 09:20:41 -0600

    Added multiplayer scripting

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,9 +4,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
 
 // MonoBehavior is the base class from which every Unity Script Derives
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     public float speed = 25.0f;
     public float rotationSpeed = 90;
@@ -19,21 +21,30 @@ public class PlayerMovement : MonoBehaviour
 
     bool isInMotion = false;
 
+    public Transform holdPoint; // Assign a point where picked objects will be parented
+
+    [SerializeField]
+    public GameObject heldObject;
+    public Transform cameratransform;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         t = GetComponent<Transform>();
         animator = GetComponent<Animator>();
+        if(!IsOwner){
+            transform.Find("Camera").GetComponent<Camera>().enabled = false;
+        }
     }
 
-    // Update is called once per frame
+    
+
     void Update()
     {
-        // animator.SetBool("IsMoving",false);
-        // animator.SetBool("IsMoving",false);
-        // Time.deltaTime represents the time that passed since the last frame
-        //the multiplication below ensures that GameObject moves constant speed every frame
+        if(!IsOwner) return;
+        
         if (Input.GetKey(KeyCode.W))
             rb.velocity += this.transform.forward * speed * Time.deltaTime;
    
@@ -59,5 +70,85 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("IsMoving", true);
         else
             animator.SetBool("IsMoving",false);
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (heldObject == null)
+                TryPickupObject();
+            else
+                DropObject();
+        }
+    }
+
+
+    /*void TryPickupObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cameratransform.position, cameratransform.forward, out hit, 2000f)) // 2f is the pickup distance
+        {
+            //Debug.Log(hit.collider.gameObject.name);
+            if (hit.collider.CompareTag("PickupObject")) // Make sure your prefabs have this tag
+            {
+                heldObject = hit.collider.gameObject;
+                heldObject.transform.SetParent(holdPoint);
+                heldObject.transform.position = holdPoint.position;
+                heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                heldObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+    }
+
+    void DropObject()
+    {
+        heldObject.transform.SetParent(null);
+        heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        heldObject.GetComponent<Collider>().enabled = true;
+        heldObject = null;
+    }*/  
+
+
+    [ServerRpc]
+    private void PickupObjectServerRpc(ulong clientId){
+        if(NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(clientId, out NetworkObject networkObject)){
+            networkObject.transform.SetParent(holdPoint);
+            networkObject.GetComponent<Rigidbody>().isKinematic = true;
+            networkObject.GetComponent<Collider>().enabled = false;
+        }
+    }
+
+
+    [ServerRpc]
+    void DropObjectServerRpc(ulong clientId)
+    {
+        if(NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(clientId, out NetworkObject networkObject)){
+            networkObject.transform.SetParent(null);
+            networkObject.GetComponent<Rigidbody>().isKinematic = false;
+            networkObject.GetComponent<Collider>().enabled = true;
+        }
+    }
+
+    void TryPickupObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cameratransform.position, cameratransform.forward, out hit, 2000f)) // Adjusted distance for realism
+        {
+            if (hit.collider.CompareTag("PickupObject"))
+            {
+                ulong objectId = hit.collider.gameObject.GetComponent<NetworkObject>().NetworkObjectId;
+                heldObject = hit.collider.gameObject;
+                PickupObjectServerRpc(objectId); // Call ServerRpc instead of direct reparenting
+            }
+        }
+    }
+
+    void DropObject()
+    {
+        if (heldObject != null)
+        {
+            ulong objectId = heldObject.GetComponent<NetworkObject>().NetworkObjectId;
+            heldObject = null;
+            DropObjectServerRpc(objectId); // Call ServerRpc for dropping the object
+        }
     }
 }
